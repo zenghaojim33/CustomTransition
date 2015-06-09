784b0974b4abb7b53853aebc35886a0a151f2558
zenghaojim33 <zenghaojim33@qq.com>
2015-06-09 09:52:22 +0800

    Initial Commit

---
+++ BubbleTransition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BubbleTransition
+//
+//  Created by Anson on 15/6/9.
+//  Copyright (c) 2015年 Anson Tsang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BubbleTransition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BubbleTransition
+//
+//  Created by Anson on 15/6/9.
+//  Copyright (c) 2015年 Anson Tsang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
